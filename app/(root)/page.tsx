e1d96291602728163f7ae99f5e5e5c09f15cbcc4
Shloimi Minkowicz <shloimiminkowicz@Shloimis-MacBook-Pro.local>
2024-12-24 14:24:37 -0600

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <>Ishop</>;
+};
+
+export default HomePage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Ishop";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "A modern ecommerce platform built with Next.js";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://locahost:3000";
