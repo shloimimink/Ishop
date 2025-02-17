9bf98bb4887852d4aa6cd33826d43d3717b4d392
shloimimink <shloimiminkowicz@gmail.com>
2025-02-16 20:19:46 -0600

    finished and completed the admin panel

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -33,12 +33,22 @@ const AdminProductsPage = async (props: {
     category,
   });
 
-  console.log(products);
-
   return (
     <div className="space-y-2">
       <div className="flex-between">
-        <h1 className="h2-bold">Products</h1>
+        <div className="flex items-center gap-3">
+          <h1 className="h2-bold">Products</h1>
+          {searchText && (
+            <div>
+              Filtered by <i>&quot;{searchText}&quot;</i>{" "}
+              <Link href="/admin/products">
+                <Button variant="outline" size="sm">
+                  Remove Filter
+                </Button>
+              </Link>
+            </div>
+          )}
+        </div>
         <Button asChild variant="default">
           <Link href="/admin/products/create">Create Product</Link>
         </Button>
