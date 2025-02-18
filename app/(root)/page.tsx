205e621a16a7fff278a198e19fc91197a9695879
shloimimink <shloimiminkowicz@gmail.com>
2025-02-18 16:00:11 -0600

    completed search and filtering

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
+import ProductCarousel from "@/components/shared/product/product-carousel";
+import ViewAllProductsButton from "@/components/view-all-products-button";
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
-    <ProductList data={latestProducts} title="Newest arrivals" limit={4} />
+    <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+      <ProductList data={latestProducts} title="Newest arrivals" limit={4} />
+      <ViewAllProductsButton />
+    </>
   );
 };
 
