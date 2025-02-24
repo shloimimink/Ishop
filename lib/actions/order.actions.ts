a9650920e2ba8cd3eed14695ff95932215f51984
shloimimink <shloimiminkowicz@gmail.com>
2025-02-23 20:03:05 -0600

    added webhook

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -206,7 +206,7 @@ export async function approvePayPalOrder(
 }
 
 // Update order to paid
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
