d9f24865347af9124c32bf91276c44a89d822cf8
shloimimink <shloimiminkowicz@gmail.com>
2025-02-23 21:46:37 -0600

    fixed order id page

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import { getOrderById } from "@/lib/actions/order.actions";
-import { notFound, redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 import OrderDetailsTable from "./order-details-table";
 import { ShippingAddress } from "@/types";
 import { auth } from "@/auth";
@@ -21,11 +21,6 @@ const OrderDetailsPage = async (props: {
   if (!order) notFound();
 
   const session = await auth();
-
-  // Redirect the user if they don't own the order
-  if (order.userId !== session?.user.id) {
-    return redirect("/unauthorized");
-  }
 
   let client_secret = null;
 
