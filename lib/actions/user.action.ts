a23498feb3896444ee2b0b3d7deab27b889b5aa5
shloimimink <shloimiminkowicz@gmail.com>
2025-02-24 20:29:41 -0600

    fixed user cart on logout

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -17,6 +17,7 @@ import { z } from "zod";
 import { Prisma } from "@prisma/client";
 import { PAGE_SIZE } from "../constants";
 import { revalidatePath } from "next/cache";
+import { getMyCart } from "./cart.actions";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -42,6 +43,8 @@ export async function signInWithCredentials(
 
 // Sign user out
 export async function signOutUser() {
+  const currentCart = await getMyCart();
+  await prisma.cart.delete({ where: { id: currentCart?.id } });
   await signOut();
 }
 
